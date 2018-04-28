6019a9a15cbb3360063b1f2256d7de2e1872b973
आनंद भैरव नाथ <anandabhairavnath@gmail.com>
2018-04-28 13:45:13 +0530

    Updated

--- tools/getfulltweets.py
+++ tools/getfulltweets.py
@@ -6,16 +6,28 @@ Created on Thu Apr 26 19:51:11 2018
 @author: ananda
 """
 
-import urllib2,pandas
+import urllib2,pandas,sys
+sys.path.append("/opt/hindi-tokenizer")
+#from HindiTokenizer import Tokenizer
 from bs4 import BeautifulSoup
-textdf=pandas.read_csv("alltweets.csv",encoding="utf-8")
+textdf=pandas.read_csv("alltweetssomefulls.csv",encoding="utf-8")
 for index,t in textdf.iterrows():
-    tb=unicode(t.text).split(" ")
-    for x in tb:
-        if "http" in x:
-            page=urllib2.urlopen(x).read()
-            bs=BeautifulSoup(page)
-            mydivs = bs.findAll("div", {"class": "tweet-text"}) 
-            if len(mydivs)>0:
-                print unicode(mydivs[0])
-                textdf.at[index,'fulltext']=unicode(mydivs[0])
+    if pandas.isnull(t['fulltext']):
+        #tokenizer=Tokenizer(t['text'].encode("utf-8"))
+        #tokenizer.tokenize()
+        tb=t['text'].split(" ")
+        for x in tb:
+            if "http" in x:
+                if "\n" in x:
+                    x="https"+x.split("https")[1]
+                print index,x
+                try:
+                    page=urllib2.urlopen(x).read()
+                    print page
+                    bs=BeautifulSoup(page)
+                    mydivs = bs.findAll("div", {"class": "permalink"}) 
+                    if len(mydivs)>0:
+                        print unicode(mydivs[0])
+                        textdf.at[index,'fulltext']=unicode(mydivs[0])
+                except:
+                    print "Oops!"

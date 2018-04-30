79b0cd23ba9c21421ee720a4ddb6c232371979a5
आनंद भैरव नाथ <anandabhairavnath@gmail.com>
2018-04-30 18:32:44 +0530

    Updated

--- tools/getfulltweets.py
+++ tools/getfulltweets.py
@@ -5,29 +5,42 @@ Created on Thu Apr 26 19:51:11 2018
 
 @author: ananda
 """
-
 import urllib2,pandas,sys
 sys.path.append("/opt/hindi-tokenizer")
-#from HindiTokenizer import Tokenizer
 from bs4 import BeautifulSoup
-textdf=pandas.read_csv("alltweetssomefulls.csv",encoding="utf-8")
-for index,t in textdf.iterrows():
-    if pandas.isnull(t['fulltext']):
-        #tokenizer=Tokenizer(t['text'].encode("utf-8"))
-        #tokenizer.tokenize()
-        tb=t['text'].split(" ")
-        for x in tb:
-            if "http" in x:
-                if "\n" in x:
-                    x="https"+x.split("https")[1]
-                print index,x
-                try:
-                    page=urllib2.urlopen(x).read()
-                    print page
+
+def get_full_tweets(textdf,startindex=0):
+    for index,t in textdf.iterrows():
+        if index>=startindex:
+            tb=t['text'].split(" ")
+            for x in tb:
+                if "http" in x:
+                    if "\n" in x:
+                        x="https"+x.split("https")[1]
+                    print index,x
+                    #try:
+                    try:
+                        page=urllib2.urlopen(x).read()
+                    #print page
+                    except:
+                        page=""
                     bs=BeautifulSoup(page)
-                    mydivs = bs.findAll("div", {"class": "permalink"}) 
+                    mydivs = bs.findAll("div", {"class": "permalink-in-reply-tos"}) + bs.find_all("div",{"class":"permalink-tweet"})
+                    fulltext=""
                     if len(mydivs)>0:
-                        print unicode(mydivs[0])
-                        textdf.at[index,'fulltext']=unicode(mydivs[0])
-                except:
-                    print "Oops!"
+                        for div in mydivs:
+                            fulltext=fulltext+div.get_text()
+                    fulltext=fulltext.replace("  "," ").replace("\n"," ").replace("Verified account"," ").replace("Unblock", "").replace("Unfollow", "").replace("Blocked", "").replace("Pending", "").replace("Show this thread", "").replace("Following","").replace("Follow","")
+                    print fulltext
+                    textdf.at[index,'fulltext']=fulltext.encode("utf-8")
+                    textdf.to_csv("alltweetssomefulls.csv",encoding="utf-8",index=False)
+                    print "Completed processing record " + str(index)
+                    
+
+
+
+
+
+
+textdf=pandas.read_csv("alltweetssomefulls.csv",encoding="utf-8")
+ 
